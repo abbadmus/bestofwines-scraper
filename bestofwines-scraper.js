7216165d2a3324232263be235b0138a15199f147
abbadmus <abdulbadmus75@gmail.com>
2020-12-20 20:29:01 +0100

    first commit

--- bestofwines-scraper.js
+++ bestofwines-scraper.js
@@ -120,6 +120,7 @@ async function scrollToBottom(page) {
   // { headless: false }
   const browser = await puppeteer.launch({
     headless: true,
+    args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
   const allLinks = await getAllLinks(page);
